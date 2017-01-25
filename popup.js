fb128de471e887cd0bb29d48f44dd1652195965d
Rizvi Iqubal <rizviiqubal@gmail.com>
2017-01-25 17:50:40 +0400

    Chrome Extension Initial Commit - Beta This adds a chrome extension to your chrome browser This helps to clear all service workers from chrom and reload page

---
+++ popup.js
@@ -0,0 +1,52 @@
+// Copyright (c) 2017 Prototype.net
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    callback(url);
+  });
+}
+
+
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(url) {
+    // Put the image URL in Google search.
+    renderStatus('Clearing All Service Workers Attached To ' + url);
+    var callback = function () {
+        renderStatus('Cleared And Reloading...');
+        chrome.tabs.getSelected(null, function(tab) {
+          var code = 'window.location.reload();';
+          chrome.tabs.executeScript(tab.id, {code: code});
+        });
+      };
+
+      var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+      var oneWeekAgo = (new Date()).getTime() - millisecondsPerWeek;
+      chrome.browsingData.remove({
+        "since": oneWeekAgo
+      }, {
+        "serviceWorkers" : true
+      }, callback);
+    /*navigator.serviceWorker.getRegistrations().then(function(registrations) {
+     for(let registration of registrations) {
+      renderStatus(registration.scope);
+    } });*/
+  });
+});
